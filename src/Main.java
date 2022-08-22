8f96f3a1bc118ec51c042baa7174e109ae54bda6
Evgeniy Malygin <black2001@inbox.ru>
2022-08-22 18:19:07 +0300

    Выполнено задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //Задание 1
         int clientOS = 0;
+        System.out.println("================Задание 1======================");
         switch (clientOS) {
             case 1: System.out.println("Установите версию приложения для Android по ссылке");
                 break;
@@ -11,6 +12,7 @@ public class Main {
         }
 
         //Задание 2
+        System.out.println("================Задание 2======================");
         int clientDeviceYear = 2016;
         if (clientDeviceYear < 2015){
             if (clientOS == 0){
@@ -25,5 +27,23 @@ public class Main {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
+
+        //Задание 3
+        System.out.println("================Задание 3======================");
+        int year = 2022;
+        if (year % 100 == 0 && year % 400 == 0)
+        {
+          System.out.println(year + " год високосный");
+        } else
+        {
+            if (year % 4 == 0 && year % 100 !=0)
+            {
+                System.out.println(year + " год високосный");
+            }else
+            {
+                System.out.println(year + " год невисокосный");
+            }
+        }
+
     }
 }
