f33da29e5cfbe6e76a071a0803e4dbe6ea052760
Evgeniy Malygin <black2001@inbox.ru>
2022-08-22 19:14:33 +0300

    Исправлено задание 2 (переделано без вложений)

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,22 @@ public class Main {
         //Задание 2
         System.out.println("================Задание 2======================");
         int clientDeviceYear = 2016;
-        if (clientDeviceYear < 2015){
-            if (clientOS == 0){
-                System.out.println("Установите облегчённую версию приложения для iOS по ссылке");
-            }else {
-                System.out.println("Установите облегчённую версию приложения для Android по ссылке");
+        if (clientDeviceYear < 2015 && clientOS == 0)
+            {
+            System.out.println("Установите облегчённую версию приложения для iOS по ссылке");
             }
-        }else {
-            if (clientOS == 0){
+        if (clientDeviceYear < 2015 && clientOS == 1)
+            {
+                System.out.println("Установите облегчённую версию приложения для Android по ссылке");
+             }
+            if (clientOS == 0 && clientDeviceYear >= 2015)
+            {
                 System.out.println("Установите версию приложения для iOS по ссылке");
-            }else {
+            }
+        if (clientOS == 1 && clientDeviceYear >= 2015)
+            {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
-        }
 
         //Задание 3
         System.out.println("================Задание 3======================");
