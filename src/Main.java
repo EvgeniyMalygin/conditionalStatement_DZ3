1e32f6b96f990c59ed58f568f7bfca4bdc3b4140
Evgeniy Malygin <black2001@inbox.ru>
2022-08-22 14:29:48 +0300

    Выполнено задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        int clientOS = 0;
+        switch (clientOS) {
+            case 1: System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            case 0: System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            default: System.out.println("Извините, приложения для вашей версии ОС не существует");
+        }
     }
 }
